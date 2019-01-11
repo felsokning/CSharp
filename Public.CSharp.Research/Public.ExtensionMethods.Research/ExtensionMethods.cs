60e0963186bf105dfb616821be6505c3642947ee
felsokning <codedebugging@outlook.ie>
2019-01-11 22:03:54 +0000

    Adding ExtensionMethods project, class, and first method to the repo.

---
+++ Public.CSharp.Research/Public.ExtensionMethods.Research/ExtensionMethods.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestInternalFactory.cs" company="None">
+//     Copyright (c) felsokning. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Public.ExtensionMethods.Research
+{
+    using System;
+    using System.Globalization;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="ExtensionMethods"/>
+    /// </summary>
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        ///     Extends the System.DateTime class to include a method to return the week number.
+        /// </summary>
+        /// <param name="dateTime">The System.DateTime object to process.</param>
+        /// <returns>An integer signifying the current week number of the year.</returns>
+        public static int Veckan(this DateTime dateTime)
+        {
+            // Jag behöver att säga tack till Peter Saverman för denna idé.
+            Calendar calendar = CultureInfo.InvariantCulture.Calendar;
+            DayOfWeek dayOfWeek = calendar.GetDayOfWeek(dateTime);
+            if (dayOfWeek >= DayOfWeek.Monday && dayOfWeek <= DayOfWeek.Wednesday)
+            {
+                dateTime = dateTime.AddDays(3);
+            }
+
+            // Vi behöver att använda måndag för den första dagen på veckan
+            // Se: https://en.wikipedia.org/wiki/ISO_week_date#Calculating_the_week_number_of_a_given_date
+            return calendar.GetWeekOfYear(dateTime, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,7 +2,19 @@
 This is C# code (often a DLL) for reference/example.
 
 ## Public.Debugging.Research
-Two PowerShell Commands (Debug-CrashDump and Debug-LiveProcess) written to dump the threads from either a crash dump file or a live process. The module is written to be directly imported into PowerShell (e.g.: Import-Module Public.Debugging.Research.dll).
+The module is written to be directly imported into PowerShell (e.g.: Import-Module Public.Debugging.Research.dll -Verbose).
 
-## Public.Exchange.Tools
-DLL meant to be imported into PowerShell and performs mass commands in parallel against an array of servers.
+### DebugDumpFile
+PowerShell Command (Debug-CrashDump) written to dump the threads from a dump file.
+
+### DebugLiveProcess
+PowerShell Command (Debug-LiveProcess) written to dump the threads from a live process.
+
+## Public.Exchange.Research
+DLL meant to be imported into PowerShell via a PowerShell Script, which performs mass commands in parallel against an array of servers.
+
+## Public.ExtensionMethods.Research
+DLL/Class meant to be imported to extend types in .NET
+
+### Veckan
+Extends the System.DateTime type to return the [ISO week number](https://en.wikipedia.org/wiki/ISO_week_date#Calculating_the_week_number_of_a_given_date) from a given date.
